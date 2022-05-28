103537e781386240c1cec5d20985c46e7b1f3225
addacub <addacu@gmail.com>
2022-05-29 08:29:06 +0930

    Initial commit

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	let hints: number = 1;
+	let solution: number;
+
+	let solutions: Number[] = [1, 2, 3, 4, 5];
+</script>
+
+<div class="slide-container">
+	<p>Select how many pieces to show:</p>
+	<input bind:value={hints} type="range" min="0" max="8" step="1" />
+	<output><b>{hints}</b></output>
+</div>
+
+<div class="spinner-container">
+	<p>Select solution to show:</p>
+	<input
+		type="number"
+		bind:value={solution}
+		min="1"
+		max={solutions ? solutions.length : 0}
+		disabled={!solutions}
+	/>
+	{#if solutions}
+		<p>{solutions.length} solutions were found.</p>
+	{:else}
+		<p>No solutions to select from.</p>
+	{/if}
+</div>
+
+<div class="button-container">
+	<button type="button"> Solve! </button>
+</div>
+
+<style>
+	div {
+		padding-top: 5rem;
+	}
+
+	input {
+		display: block;
+		margin: auto;
+	}
+
+	output {
+		text-align: center;
+		color: white;
+		display: block;
+		padding-top: 15px;
+	}
+
+	button {
+		background-color: #c8adc0;
+		width: 6em;
+		height: 2em;
+		font-size: xx-large;
+		border-width: 5px;
+	}
+
+	button:hover {
+		background-color: hsl(318, 20%, 50%);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Calendar from '$lib/Calendar.svelte';
+	import Menu from '$lib/Menu.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="row">
+	<div class="column left">
+		<Menu />
+	</div>
+	<div class="column right">
+		<Calendar />
+	</div>
+</section>
+
+<style>
+	/* section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 2;
+	} */
+
+	.row {
+		display: flex;
+	}
+
+	.column {
+		float: left;
+	}
+
+	.left {
+		width: 25%;
+		margin-right: 10rem;
+	}
+
+	.right {
+		width: 75%;
+	}
+</style>
