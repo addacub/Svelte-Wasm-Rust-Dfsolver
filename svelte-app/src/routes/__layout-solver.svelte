20876cfe9c3b3ed08b79903f6cfa4bd1c097f38f
addacub <addacu@gmail.com>
2022-07-26 21:12:38 +0930

    Add media querys to solver
    This commit adds media querys to the solver page so that the layout is
    adjusted (as best as possible) for different screen sizes and
    orientations. Note, the ideal screen is that of a computer but a
    handheld device such as a phone or ipad will also suffice.

---
+++ svelte-app/src/routes/__layout-solver.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: fit-content;
+		margin: 0 auto;
+		box-sizing: border-box;
+		background-color: 19232D;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (max-height: 760px) {
+		footer {
+			padding: 0px 0;
+			font-size: small;
+		}
+	}
+</style>
