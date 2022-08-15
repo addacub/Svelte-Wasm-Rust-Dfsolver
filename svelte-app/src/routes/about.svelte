730ba79a7e5f294e27a13d424bcdd3fe984db5d2
addacub <addacu@gmail.com>
2022-08-15 16:34:35 +0930

    Switched order of mp4 and webm video and rebuilt

--- svelte-app/src/routes/about.svelte
+++ svelte-app/src/routes/about.svelte
@@ -721,8 +721,8 @@
 		<div class="lazy_container">
 			<Lazy fadeOption={{ delay: delay, duration: duration }}>
 				<video width="100%" height="100%" autoplay muted controls loop>
-					<source src={graphStream_mp4} type="video/mp4" />
 					<source src={graphStream_webm} type="video/webm" />
+					<source src={graphStream_mp4} type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
 			</Lazy>
